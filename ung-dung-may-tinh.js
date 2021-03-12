c4a85f16196252f0eb9aae433b35dc99f95a8592
duyhai97 <79962575+duyhai97@users.noreply.github.com>
2021-03-12 17:18:11 +0700

    cau trau dieu kien

--- ung-dung-may-tinh.js
+++ ung-dung-may-tinh.js
@@ -1,17 +1,17 @@
 function ketqua(type) {
-    let sothunhat = document.getElementById('AA').value;
-    let sothuhai = document.getElementById('BB').value;
+    let sothunhat = +document.getElementById('AA').value;
+    let sothuhai = +document.getElementById('BB').value;
     let convert;
 
     if (type === 'cong') {
-        convert = parseInt(sothunhat) + parseInt(sothuhai);
+        convert = sothunhat + sothuhai;
     } else if (type === 'tru') {
-        convert = parseInt(sothunhat) - parseInt(sothuhai);
+        convert =sothunhat -sothuhai;
     } else if (type === 'nhan') {
-        convert = parseInt(sothunhat) * parseInt(sothuhai);
+        convert = sothunhat * sothuhai;
 
     } else {
-        convert = parseInt(sothunhat) / parseInt(sothuhai);
+        convert = sothunhat / sothuhai;
     }
 
     document.getElementById('result').innerHTML = 'Result: ' + convert;
